7a968567d64c77627cf70d49151edd1491204ff4
hubbacrack <jeremy.huxleyr@gmail.com>
2022-02-01 13:04:13 +0000

    Fixed bugs

--- Hello.py
+++ Hello.py
@@ -1,5 +1,8 @@
 print('HelloWorld')
 print("Something else")
 
-x = input("How are you?:")
-print("I do not care")
+x = input("How are you? (good/bad):")
+if x == "good":
+    print("Nice to hear that.")
+elif x == "bad":
+    print("lololol stay sad")
