974d65b4152632b525822a3d43afee0b351486ee
DanLeech1 <D.Leech@2021.ljmu.ac.uk>
2022-02-08 13:29:49 +0000

    poggers

---
+++ Movietickets.py
@@ -0,0 +1,12 @@
+
+while(True):
+    lol = int(input('What is your age bro'))
+
+    if(lol < 3):
+        print('Woah you get in for free')
+
+    elif(lol <= 12 and lol >= 3):
+        print('Ticket costs 10 quid mate')
+
+    elif(lol > 12):
+        print('Thatll be 15 quid my guy')
